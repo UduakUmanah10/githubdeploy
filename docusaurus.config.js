e84990201bc0ba288a29e67d7a4fe5d21a0a99d3
uduak umanah <uduakumanah10@gmail.com>
2024-06-12 20:24:36 +0100

    set up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,18 +13,19 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://UduakUmanah10.github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/githubdeploy/',
+  trailingSlash:false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'UduakUmanah10', // Usually your GitHub org/user name.
+  projectName: 'githubdeploy', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
